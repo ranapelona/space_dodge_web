3694b1984d9aff01a99dc0528abc61670b46fcc2
Aldo <aldomacias20@gmail.com>
2026-04-20 02:19:19 -0700

    Update main.py

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ PLAYER_VEL = 6
 STAR_WIDTH = 10
 STAR_HEIGHT = 20
 STAR_VEL = 2
-PLAYER_IMG_RAW = pygame.image.load("kid.png").convert_alpha()
+PLAYER_IMG_RAW = pygame.image.load("kid.png")
 PLAYER_IMG = pygame.transform.scale(
     PLAYER_IMG_RAW, (PLAYER_WIDTH, PLAYER_HEIGHT))
 FONT = pygame.font.SysFont("Arial", 30)
